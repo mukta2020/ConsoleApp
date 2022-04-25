5f4cc9992ddd85b532e06d401565a80303f6c2c9
mukta2020 <66303108+mukta2020@users.noreply.github.com>
2022-04-25 10:26:38 +0600

    device 4

--- CodingConsoleApp/Program.cs
+++ CodingConsoleApp/Program.cs
@@ -3941,6 +3941,136 @@ namespace CodingConsoleApp
                 superLockingCell = new string[0];
                 return new Tuple<string[], string[], string[], string[]>(superCell, lockingCell, superLockingCell, stdCell);
             }
+            else if (id == 4)// for max2 132/33/2  
+            {
+                allCell = new string[aToz.Length * 8 + 2]; // 176 + 2 = 178
+                superCell = new string[aToz.Length]; //22 
+                lockingCell = new string[aToz.Length]; // 22                           
+
+                if (customSuperQuantity > 0)
+                {
+                    superLockingCell = new string[customSuperQuantity];
+                    superCell = new string[aToz.Length * 2 - customSuperQuantity];
+                    string[] firstArray; string[] secondArray;
+
+                    stdCell = new[] {      "A1","B1","C1","D1","E1","F1","G1","H1","I1","J1","K1","L1","M1","N1","O1","P1","Q1","R1","S1","T1","U1","V1",
+                                           "A2","B2","C2","D2","E2","F2","G2","H2","I2","J2","K2","L2","M2","N2","O2","P2","Q2","R2","S2","T2","U2","V2",                                         
+                                           "A7","B7","C7","D7","E7","F7","G7","H7","I7","J7",  "K7","L7","M7","N7","O7","P7","Q7","R7","S7","T7","U7","V7",
+                                           "A8","B8","C8","D8","E8","F8","G8", "H8","I8","J8","K8","L8","M8","N8","O8","P8","Q8", "R8","S8","T8","U8","V8"
+                                            };
+
+
+                    lockingCell = new[] { "A9","B9","C9","D9","E9","F9","G9","H9","I9","J9","K9","L9","M9","N9","O9","P9","Q9","R9","S9","T9","U9","V9" };
+
+                    if (customSuperQuantity == 5)
+                    {
+
+                        superCell = new[] { "A4","B4","C4", "D4","E4","F4", "G4","H4","I4", "J4","K4", "L4", "M4", "N4","O4","P4", "Q4",
+                                            "A6","B6","C6","D6","E6","F6", "G6", "H6", "I6", "J6", "K6","L6", "M6", "N6","O6","P6", "Q6" };  // same as Standard
+
+                        superLockingCell = new[] {"R4","S4", "T4","U4", "V4" };
+
+
+                    }
+                    else if (customSuperQuantity == 10)
+                    {
+
+                        superCell = new[] { "A4","B4","C4", "D4","E4","F4", "G4","H4","I4", "J4","K4", "L4", 
+                                            "A6","B6","C6","D6","E6","F6", "G6", "H6", "I6", "J6", "K6","L6", "M6", "N6","O6","P6", "Q6" };  // same as Standard
+
+                        superLockingCell = new[] { "M4", "N4", "O4", "P4", "Q4", "R4", "S4", "T4", "U4", "V4" };
+
+                    }
+                    else if (customSuperQuantity == 15)
+                    {
+
+
+                        superCell = new[] { "A4","B4","C4", "D4","E4","F4", "G4",
+                                            "A6","B6","C6","D6","E6","F6", "G6", "H6", "I6", "J6", "K6","L6", "M6", "N6","O6","P6", "Q6" };  // same as Standard
+
+                        superLockingCell = new[] { "H4", "I4", "J4", "K4", "L4", "M4", "N4", "O4", "P4", "Q4", "R4", "S4", "T4", "U4", "V4" };
+
+                    }
+                    else
+                    {
+                        firstArray = new string[0];
+                        secondArray = new string[0];
+                    }
+
+                    // SuperQuantityCellCalulationForTwo(firstArray, secondArray, superLockingCell, superCell);
+                    // stdCell = allCell.Except(superCell).Except(lockingCell).Except(superLockingCell).ToArray();
+
+                    return new Tuple<string[], string[], string[], string[]>(superCell, lockingCell, superLockingCell, stdCell);
+                }
+
+                if (customStandardQuantity > 0)
+                {
+                    superLockingCell = new string[0];
+                    lockingCell = new string[aToz.Length + customStandardQuantity]; // 22 + 15
+
+                    superCell = new[] { "A4","B4","C4", "D4","E4","F4", "G4","H4","I4", "J4","K4", "L4", "M4", "N4","O4","P4", "Q4","R4","S4", "T4","U4","V4",
+                                        "A6","B6","C6","D6","E6","F6","G6","H6","I6", "J6","K6" , "L6","M6","N6","O6","P6","Q6","R6","S6","T6","U6","V6",};  // same as Standard
+
+
+                    if (customStandardQuantity == 5)
+                    {
+
+                        stdCell = new[] { "A1","B1","C1","D1","E1","F1","G1","H1","I1","J1","K1","L1","M1","N1","O1","P1","Q1","R1","S1","T1","U1","V1",
+                                           "A2","B2","C2","D2","E2","F2","G2","H2","I2","J2","K2","L2","M2","N2","O2","P2","Q2","R2","S2","T2","U2","V2", 
+                                            "A7","B7","C7","D7","E7","F7","G7","H7","I7","J7","K7","L7","M7","N7","O7","P7","Q7","R7","S7","T7","U7","V7",
+                                            "A8","B8","C8","D8","E8","F8","G8","H8","I8","J8","K8","L8","M8","N8","O8","P8","Q8"
+                                         };
+
+                       
+                        lockingCell = new[] { "R8","S8","T8","U8","V8","A9","B9","C9","D9","E9","F9","G9","H9","I9","J9",
+                                                 "K9","L9","M9","N9","O9","P9","Q9","R9","S9","T9","U9","V9" };
+
+                    }
+                    else if (customStandardQuantity == 10)
+                    {
+
+                        stdCell = new[] { "A1","B1","C1","D1","E1","F1","G1","H1","I1","J1","K1","L1","M1","N1","O1","P1","Q1","R1","S1","T1","U1","V1",
+                                           "A2","B2","C2","D2","E2","F2","G2","H2","I2","J2","K2","L2","M2","N2","O2","P2","Q2","R2","S2","T2","U2","V2",
+                                            "A7","B7","C7","D7","E7","F7","G7","H7","I7","J7","K7","L7","M7","N7","O7","P7","Q7","R7","S7","T7","U7","V7",
+                                            "A8","B8","C8","D8","E8","F8","G8","H8","I8","J8","K8","L8"
+                                         };
+
+                        
+                        lockingCell = new[] { "M8","N8","O8","P8","Q8","R8","S8","T8","U8","V8","A9","B9","C9","D9","E9","F9","G9","H9","I9","J9",
+                                                 "K9","L9","M9","N9","O9","P9","Q9","R9","S9","T9","U9","V9" };
+
+
+                    }
+                    else if (customStandardQuantity == 15)
+                    {
+                        stdCell = new[] { "A1","B1","C1","D1","E1","F1","G1","H1","I1","J1","K1","L1","M1","N1","O1","P1","Q1","R1","S1","T1","U1","V1",
+                                           "A2","B2","C2","D2","E2","F2","G2","H2","I2","J2","K2","L2","M2","N2","O2","P2","Q2","R2","S2","T2","U2","V2",
+                                            "A7","B7","C7","D7","E7","F7","G7","H7","I7","J7","K7","L7","M7","N7","O7","P7","Q7","R7","S7","T7","U7","V7",
+                                            "A8","B8","C8","D8","E8","F8","G8"
+                                         };
+
+                       
+                        lockingCell = new[] { "H8","I8","J8","K8","L8","M8","N8","O8","P8","Q8","R8","S8","T8","U8","V8","A9","B9","C9","D9","E9","F9","G9","H9","I9","J9",
+                                                 "K9","L9","M9","N9","O9","P9","Q9","R9","S9","T9","U9","V9" };
+
+
+                    }
+                    else
+                    {
+                        stdCell = new string[0];
+                    }
+
+                    return new Tuple<string[], string[], string[], string[]>(superCell, lockingCell, superLockingCell, stdCell); // superLockingCell  null here
+                }
+
+
+
+                superCell = new string[0];
+                lockingCell = new string[0];
+                stdCell = new string[0];
+                superLockingCell = new string[0];
+                return new Tuple<string[], string[], string[], string[]>(superCell, lockingCell, superLockingCell, stdCell);
+            }
 
             else
             {
